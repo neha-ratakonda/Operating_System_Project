3b22b80f3cb4401f56f747a6ba54aed6e95582a1
jatinmd7 <152249606+jatinmd7@users.noreply.github.com>
2023-12-04 17:46:42 +0530

    Update first try.py

--- first try.py
+++ first try.py
@@ -1,9 +1,10 @@
 import threading
 
 max_passengers = 5
-mutex = threading.Semaphore(1)
+mutex = threading.Semaphore(1)#used to control access to the shared variable `passengers_in_car`. It ensures that only one thread at a time can modify the value of `passengers_in_car`.
 #onboard = threading.Semaphore(0)
-full = threading.Semaphore(0)
+full = threading.Semaphore(0)#This semaphore is used to make sure the roller coaster doesn't start moving until it is full [i.e., until the specified number of passengers(number of passengers==Capacity) have boarded].
+
 #boarding = threading.Semaphore(1) 
 
 passengers_in_car = 0
@@ -13,7 +14,7 @@ def passenger():
     
     while True:
         
-        mutex.acquire()
+        mutex.acquire()#called before entering the critical section where `passengers_in_car` is modified
         #boarding.acquire()#when passenger empty has empty seats left, then only passenger can get it
 
 #something we’re still working on
@@ -21,10 +22,10 @@ def passenger():
         passengers_in_car += 1
         print(f"Roller coaster is moving with {passengers_in_car} passengers")
         
-        mutex.release()
+        mutex.release()#called afterward to release the lock
         
         if onBoard == CAPACITY:
-            full.release()
+            full.release() #When a passenger thread increments `passengers_in_car` to the desired capacity , it releases the `full` semaphore
             
 #         else:
 #             boarding.release()
@@ -36,7 +37,7 @@ def rollercoaster():
     
     while True:
         
-        full.aquire()
+        full.aquire() #This allows the `rollercoaster` function to proceed, indicating that the roller coaster is full and can start moving.
         
         print("The roller coaster is moving wohoooo !!!!")
         
